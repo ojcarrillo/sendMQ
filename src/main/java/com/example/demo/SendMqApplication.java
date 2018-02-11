14c0e846b97fda0505e9284fb69681c57bee5ab8
ojcarrillo <oj_carrillo@yahoo.com>
2018-02-11 20:19:55 +0000

    actualizado sender a la cola mq_movctas

--- src/main/java/com/example/demo/SendMqApplication.java
+++ src/main/java/com/example/demo/SendMqApplication.java
@@ -3,9 +3,11 @@ package com.example.demo;
 import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.MessageProperties;
 
 import java.io.IOException;
 import java.util.concurrent.TimeoutException;
+import java.util.Date;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -23,16 +25,22 @@ public class SendMqApplication {
 		SpringApplication.run(SendMqApplication.class, args);
 		try {
 			ConnectionFactory factory = new ConnectionFactory();
-			factory.setUsername("test");
-			factory.setPassword("test");	    
-			factory.setHost("35.203.93.92");
+//			factory.setUsername("quest");
+//			factory.setPassword("quest");	    
+			factory.setHost("localhost");
 			factory.setPort(5672);
+			factory.setUsername("b2c_client");
+			factory.setPassword("SuperPassword000");
+//			factory.setHost("35.203.93.92");
+//			factory.setPort(5672);
+//factory.setHost("vhost1");
+
 			Connection connection = factory.newConnection();			
 			Channel channel = connection.createChannel();			
-			channel.queueDeclare(QUEUE_NAME, false, false, false, null);
-			String message = "THE JOKER WAR HERE!";
-			channel.basicPublish("", QUEUE_NAME, null, message.getBytes("UTF-8"));
-			System.out.println(" [x] Sent '" + message + "'");
+			channel.queueDeclare(QUEUE_NAME, true, false, false, null);
+			String message = "THE JOKER WAR HERE!->" + (new Date()).toString();
+			channel.basicPublish("", QUEUE_NAME, MessageProperties.PERSISTENT_TEXT_PLAIN, message.getBytes("UTF-8"));
+			System.out.println("======================> [x] Sent '" + message + "'");
 			
 			channel.close();
 			connection.close();
