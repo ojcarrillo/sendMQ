d07bb49101a481da413b4412a33b3b09d4365c7f
ojcarrillo <ojcarrillo@yahoo.com>
2018-02-12 15:23:27 -0500

    ajuste de codificacion y comentarios

--- src/main/java/com/example/demo/SendMqApplication.java
+++ src/main/java/com/example/demo/SendMqApplication.java
@@ -6,8 +6,8 @@ import com.rabbitmq.client.ConnectionFactory;
 import com.rabbitmq.client.MessageProperties;
 
 import java.io.IOException;
-import java.util.concurrent.TimeoutException;
 import java.util.Date;
+import java.util.concurrent.TimeoutException;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -21,27 +21,26 @@ public class SendMqApplication {
 
 	private final static String QUEUE_NAME = "mq_movctas";
 	
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SendMqApplication.class, args);
 		try {
-			ConnectionFactory factory = new ConnectionFactory();
-//			factory.setUsername("quest");
-//			factory.setPassword("quest");	    
-			factory.setHost("localhost");
-			factory.setPort(5672);
+			/* configura la conexion al servidor de colas */
+			ConnectionFactory factory = new ConnectionFactory();    
 			factory.setUsername("b2c_client");
 			factory.setPassword("SuperPassword000");
-//			factory.setHost("35.203.93.92");
-//			factory.setPort(5672);
-//factory.setHost("vhost1");
-
+			factory.setHost("localhost");
+			factory.setPort(5672);
+			/* abre la conexion */
 			Connection connection = factory.newConnection();			
 			Channel channel = connection.createChannel();			
 			channel.queueDeclare(QUEUE_NAME, true, false, false, null);
-			String message = "THE JOKER WAR HERE!->" + (new Date()).toString();
-			channel.basicPublish("", QUEUE_NAME, MessageProperties.PERSISTENT_TEXT_PLAIN, message.getBytes("UTF-8"));
-			System.out.println("======================> [x] Sent '" + message + "'");
-			
+			/* genera el mensaje */
+			String message = "THE JOKER WAR HERE! " + (new Date()).toString();
+			/* publica el mensaje en la cola */
+			channel.basicPublish("", QUEUE_NAME, MessageProperties.PERSISTENT_TEXT_PLAIN, message.getBytes("UTF-8"));			
+			System.out.println(" [x] Sent '" + message + "'");
+			/* cierra la conexion al servidor de colas */
 			channel.close();
 			connection.close();
 		} catch (IOException e) {
@@ -53,3 +52,4 @@ public class SendMqApplication {
 		}
 	}
 }
+
